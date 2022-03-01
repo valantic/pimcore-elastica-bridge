f8f1d1c71046e3c8d36bda2902f1754beaf5516a
Linus Metzler <linus.metzler@cec.valantic.com>
2022-03-01 17:11:12 +0100

    allow unpublished elements to be indexed

--- src/DocumentType/Index/ListingTrait.php
+++ src/DocumentType/Index/ListingTrait.php
@@ -24,6 +24,11 @@ trait ListingTrait
 
     abstract public function treatObjectVariantsAsDocuments(): bool;
 
+    public function includeUnpublishedElementsInListing(): bool
+    {
+        return false;
+    }
+
     public function getIndexListingCondition(): ?string
     {
         return null;
@@ -31,11 +36,16 @@ trait ListingTrait
 
     public function getListingInstance(IndexInterface $index): AbstractListing
     {
-        /** @var AbstractListing $listingClass */
+        /** @var class-string<AbstractListing> $listingClass */
         $listingClass = $this->getListingClass();
 
+        /** @var AbstractListing $listingInstance */
         $listingInstance = new $listingClass();
         $listingInstance->setCondition($this->getIndexListingCondition());
+
+        if (in_array($this->getType(), [DocumentInterface::TYPE_OBJECT, DocumentInterface::TYPE_DOCUMENT], true)) {
+            $listingInstance->setUnpublished($this->includeUnpublishedElementsInListing());
+        }
 
         if ($this->getType() === DocumentInterface::TYPE_OBJECT && $this->treatObjectVariantsAsDocuments()) {
             $listingInstance->setObjectTypes([DataObject\AbstractObject::OBJECT_TYPE_OBJECT, DataObject\AbstractObject::OBJECT_TYPE_VARIANT]);
