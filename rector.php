db1dc943707b0cd22c31ed02a88de429d9ca23a8
Linus Metzler <hi@linusmetzler.me>
2023-04-09 21:31:44 +0200

    formatting

--- rector.php
+++ rector.php
@@ -18,6 +18,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY
+        SetList::CODE_QUALITY,
     ]);
 };
